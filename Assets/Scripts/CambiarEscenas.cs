76ec244098dbb9624be85f650eb2a633f554a80b
EmersonQH <64039337+EmersonQH@users.noreply.github.com>
2022-10-19 13:23:34 -0500

    Añadiendo Una Escena de inicio de Prueba

---
+++ Assets/Scripts/CambiarEscenas.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscenas : MonoBehaviour
+{
+    public void EmpezarJuego(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    public void CerrarJuego(){
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+}
